d31f564bc3e2baeae9a14b17a6ee2375fc7e7988
jrbuniel <104114225+jrbuniel@users.noreply.github.com>
2024-02-07 20:40:56 +0800

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,42 @@
+import { Component, OnInit, Renderer2, Inject } from '@angular/core';
+import { faClipboard } from '@fortawesome/free-solid-svg-icons';
+import { DOCUMENT } from '@angular/common';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  faClipboard = faClipboard;
+  
+  open: boolean = false;
+  text: string = 'Happy Anniversary my love <br> haha';
+
+  ngOnInit() {
+      
+  }
+
+  // click(): void {
+  //   this.open = !this.open;
+
+  //   if (this.open) {
+
+  //   }
+  // }
+
+  nextPage(): void {
+    window.location.href = "yes.html";
+  }
+
+  moveButton(): void {
+    var noButton = document.getElementById('noButton') as HTMLElement;
+
+    var x = Math.random() * (window.innerWidth - noButton.offsetWidth) - 85;
+    var y = Math.random() * (window.innerHeight - noButton.offsetHeight) - 48;
+
+    noButton.style.left = `${x}px`;
+    noButton.style.top = `${y}px`;
+  }
+}
---
+++ src/app/app.module.ts
@@ -0,0 +1,29 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { DragDropModule } from '@angular/cdk/drag-drop';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { MatDialogModule } from '@angular/material/dialog';
+import { FormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    DragDropModule,
+    FontAwesomeModule,
+    MatDialogModule,
+    FormsModule,
+    MatFormFieldModule,
+    MatInputModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
